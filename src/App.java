a9bba5846b1a8473f528dea47fd6a1472c790295
Sophia Dronova <sophiadronova@tamu.edu>
2025-04-20 15:55:49 -0500

    main panel with title

--- src/App.java
+++ src/App.java
@@ -1,7 +1,33 @@
 import javax.swing.*;
+import java.awt.Color;
+import java.awt.Font;
 
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+    public static void main(String[] args) {
+        Color maroon = new Color(128, 0, 0);
+
+
+        JFrame frame = new JFrame("Voting Form");
+        frame.setBackground(Color.WHITE);
+        frame.setSize(600, 800);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        panel.setBackground(Color.WHITE);
+
+        JPanel titlePanel = new JPanel();
+        titlePanel.setLayout(new BoxLayout(titlePanel, BoxLayout.Y_AXIS));
+        titlePanel.setBackground(Color.WHITE);
+        JLabel titleLabel = new JLabel("Voting Form");
+        titleLabel.setFont(new Font("Arial", Font.BOLD, 30));
+        titleLabel.setForeground(maroon);
+        titlePanel.add(titleLabel);
+        panel.add(titlePanel);
+
+        frame.add(panel);
+        frame.setVisible(true);
+
     }
 }

3d801c29af475b429cda7b2fb1a1fc5e66bfdc55
Sophia Dronova <sophiadronova@tamu.edu>
2025-04-25 16:50:01 -0500

    embedded documents works

--- src/SubmitVote.java
+++ src/SubmitVote.java
@@ -1,4 +1,8 @@
 import java.awt.Color;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 import javax.swing.JComboBox;
 import javax.swing.JOptionPane;
@@ -15,20 +19,37 @@ public class SubmitVote {
         String id = app.idField.getText().trim();
         String pin = app.pinField.getText().trim();
 
-
         if (id.isEmpty() || pin.isEmpty() || id.equals("Enter your Voter ID") || pin.equals("Enter your Registration PIN")) {
             JOptionPane.showMessageDialog(app.panel, "Please enter a valid Voter ID and PIN");
             return;
         }
 
-        String first = (String) app.dropdowns[0].getSelectedItem();
-        String second = (String) app.dropdowns[1].getSelectedItem();
-        String third = (String) app.dropdowns[2].getSelectedItem();
-
-        if (first.startsWith("Select") || second.startsWith("Select") || third.startsWith("Select")) {
-            JOptionPane.showMessageDialog(app.panel, "Please select your top three choices");
-            return;
+        String[] nomineeIDs = {"n1", "n2", "n3"};
+        String[] choices = new String[3];
+        for (int i = 0; i < 3; i++) {
+            choices[i] = (String) app.dropdowns[i].getSelectedItem();
+            if (choices[i].startsWith("Select")) {
+                JOptionPane.showMessageDialog(app.panel, "Please select all three choices");
+                return;                
+            }
         }
+
+        Map<String, String> partyNameMap = Map.of(
+            "Taylor Swift", "Freedom",
+            "Mr. Bean", "Comedy",
+            "George Washington", "President",
+            "Abraham Lincoln", "President",
+            "William Henry Harrison", "Freedom"
+        );
+
+        List<Document> rankings = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            String name = choices[i];
+            String party = partyNameMap.get(name);
+
+            rankings.add(new Document("rank", i + 1).append("nominee", new Document("nomineeID", nomineeIDs[i]).append("party", party)));
+        }
+
 
         String uri = "mongodb+srv://sophiadronova:csce310team11@cluster0.btrged1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
         try (MongoClient mongoClient = MongoClients.create(uri)) {
@@ -45,7 +66,11 @@ public class SubmitVote {
             }
 
             // otherwise, insert vote into db
-            Document vote = new Document("id", id).append("pin", pin).append("firstChoice", first).append("secondChoice", second).append("thirdChoice", third);
+            String electionID = "2025General", electionName = "2025 General Election", electionDate = "2023-04-03T00:00:00Z";
+            Document vote = new Document("voter", new Document("voterID", id).append("regPIN", pin))
+            .append("election", new Document("electionID", electionID).append("name", electionName).append("date", electionDate))
+            .append("timestamp", Instant.now().toString())
+            .append("rankings", rankings);
             votes.insertOne(vote);
 
             app.pinField.setText("Enter your Registration PIN");
